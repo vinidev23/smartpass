f675200d74eed335fea9d76bc2fe07d566309315
vinidev23 <vinidev23>
2025-12-03 20:23:39 -0300

    Adicionando o check log para dados do usuário ao scannear qr code

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timezone
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 engine = create_engine("sqlite:///smartpass.db", connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
@@ -16,6 +16,17 @@ class User(Base):
     qr_code_path = Column(String, nullable=False)
     last_checkin = Column(DateTime, nullable=True)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    
+    logs = relationship("Checklog", back_populates="user")
+    
+class CheckLog(Base):
+     __tablename__ = "check_logs"
+     id = Column(Integer, primary_key=True)
+     user_id = Column(Integer, ForeignKey("users.id"))
+     action = Column(String)
+     timestamp = Column(DateTime, default=datetime.utcnow)
+     
+     user = relationship("User", back_populates="logs")
 
 def init_db():
     Base.metadata.create_all(engine)
